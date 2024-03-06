4fdabe4abcf5ab7e2052e8e485276e4f04dd737a
sundharesan <rahul@gamail.com>
2024-03-06 07:39:05 +0000

    commit message

---
+++ src/Components/Login/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="">
+        <h1 className="">sdjsjds</h1>
+        <p>dfdfjdfjgjfgjfdk</p>
+      </div>
+    )
+  }
+}
+
+export default Login
